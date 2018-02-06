b069d5d81ac02bbcde6b0e1cf9cbc69bed4cced6
wo <80504034@qq.com>
2018-02-06 22:57:08 +0800

    first blog

---
+++ testpython/01.py
@@ -0,0 +1,14 @@
+import tornado.web
+import tornado.ioloop
+
+
+class IndexHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.write("hello world")
+
+
+if __name__ == '__main__':
+    app = tornado.web.Application([(r"/", IndexHandler)])
+    app.listen(8000)
+    tornado.ioloop.IOLoop.current().start()
